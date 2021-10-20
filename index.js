d012e374966f1de095d364698605f3a1d893329a
Felipe Santos <felipecassiors@gmail.com>
2021-10-20 00:10:00 -0300

    Setup prettier, add exit handle

--- index.js
+++ index.js
@@ -1,56 +1,108 @@
-import clipboardListener from 'clipboard-event';
-import clipboard from 'clipboardy';
-import chokidar from 'chokidar';
-import fs from 'fs';
-import path from 'path';
+import clipboardListener from "clipboard-event";
+import clipboard from "clipboardy";
+import chokidar from "chokidar";
+import fs from "fs";
+import path from "path";
+import { stderr } from "process";
 
-// To start listening
+const folder = "clipboard-sync";
+
+let lastTextWritten = "";
+let lastTimeWritten = "";
+
+let lastTextRead = "";
+let lastTimeRead = "";
+
+// Handles exiting the program
+[
+  "exit",
+  "SIGINT",
+  "SIGUSR1",
+  "SIGUSR2",
+  // 'uncaughtException',
+  "SIGTERM",
+].forEach((eventType) => {
+  process.on(eventType, () => {
+    clipboardListener.stopListening();
+    process.exit();
+  });
+});
+
 clipboardListener.startListening();
 
-const folder = 'clipboard-sync'
+// Writes clipboard to file
+clipboardListener.on("change", () => {
+  let textToWrite = "";
+  try {
+    textToWrite = clipboard.readSync();
+  } catch (error) {
+    console.error("Error reading clipboard");
+    return;
+  }
+  if (
+    !textToWrite ||
+    lastTextRead === textToWrite ||
+    lastTextWritten === textToWrite
+  ) {
+    return;
+  }
 
-let lastTextRead = ''
-let lastTextWritten = ''
-let lastTimeWritten = ''
+  const writeTime = `${Date.now()}`;
+  const filePath = path.join(folder, `${writeTime}.txt`);
 
-clipboardListener.on('change', () => {
-    const text = clipboard.readSync();
+  lastTimeWritten = writeTime;
+  lastTextWritten = textToWrite;
 
-    if (!text || lastTextRead === text || lastTextWritten === text) {
-        return
-    }
-
-    lastTextWritten = text
-
-    const timestamp = Date.now().toString()
-    lastTimeWritten = timestamp
-
-    const file = path.join(folder, `${timestamp}.txt`);
-    console.log(`Writing clipboard to ${file}`);
-
-    fs.writeFileSync(file, text, {
-        encoding: 'utf8'
-    });
+  console.log(`Writing clipboard to ${filePath}`);
+  fs.writeFileSync(filePath, textToWrite, {
+    encoding: "utf8",
+  });
 });
 
-chokidar.watch(`${folder}/*.txt`, {
+// Watches for files and reads clipboard from it
+chokidar
+  .watch(`${folder}/*.txt`, {
     ignoreInitial: true,
-}).on('add', (filePath) => {
+    depth: 1,
+  })
+  .on("add", (filePath) => {
     const currentText = clipboard.readSync();
+
     const newText = fs.readFileSync(filePath, {
-        encoding: 'utf8'
-    })
+      encoding: "utf8",
+    });
 
-    if (newText && currentText !== newText) {
-        console.log(`Reading clipboard from ${filePath}`);
-        lastTextRead = newText
-        const timestamp = path.parse(filePath).name
-        if (lastTimeWritten && timestamp <= lastTimeWritten) {
-            return
-        }
-        clipboard.writeSync(newText);
+    // Prevents writing duplicated text to clipboard
+    if (!newText || currentText === newText) {
+      return;
     }
-});
 
-// To stop listening
-// clipboardListener.stopListening();
+    const currentFileTime = path.parse(filePath).name;
+    // Skips the read if a newer file was already wrote
+    if (lastTimeWritten && currentFileTime <= lastTimeWritten) {
+      return;
+    }
+
+    // Skips if a newer file was already read
+    if (lastTimeRead && currentFileTime <= lastTimeRead) {
+      return;
+    }
+
+    lastTextRead = newText;
+    lastTimeRead = currentFileTime;
+
+    console.log(`Reading clipboard from ${filePath}`);
+    clipboard.writeSync(newText);
+  });
+
+// Remove files older than 5 minutes
+chokidar
+  .watch(`${folder}/*.txt`, {
+    depth: 1,
+  })
+  .on("add", (filePath) => {
+    const currentTimeMinus5Min = `${Date.now() - 300000}`;
+    if (path.parse(filePath).name <= currentTimeMinus5Min) {
+      fs.unlinkSync(filePath);
+    }
+  });
